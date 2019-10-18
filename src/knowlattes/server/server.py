b953dc75058b69a9e44fce3407b828cd31f7a370
Henrique <henrique.facioli@nubank.com.br>
2019-10-18 10:14:16 -0300

    server

--- src/knowlattes/server/server.py
+++ src/knowlattes/server/server.py
@@ -22,3 +22,11 @@ def hello(query=None):
     if request.method == 'GET':
         query = request.args.get('query', '')
     return render_template('index.html', query=escape(query))
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('page_not_found.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
