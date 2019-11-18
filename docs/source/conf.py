737eeba7bb639e457f9b6387f85b6fe918ade4b3
Henrique <henrique.facioli@nubank.com.br>
2019-11-18 20:44:55 -0300

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 from datetime import datetime
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../src/knowlattes"))
 
 
 # -- Project information -----------------------------------------------------
